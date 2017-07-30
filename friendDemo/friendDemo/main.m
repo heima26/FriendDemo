084926bb374e6689f588aaecb6f5d24157a60a77
hm26 <xx@qq.com>
2017-07-30 10:41:19 +0800

    01-立项

---
+++ friendDemo/friendDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  friendDemo
+//
+//  Created by apple on 2017/7/30.
+//  Copyright © 2017年 itheima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
